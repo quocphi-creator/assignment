36782ddd3610572851038be5086373b7c15f671d
Nguyen Quoc Phi <phinqhe153034@fpt.edu.vn>
2022-02-19 18:00:54 +0700

    update bill search (in progress)

--- src/java/controler/bill/SearchControler.java
+++ src/java/controler/bill/SearchControler.java
@@ -40,7 +40,7 @@ public class SearchControler extends HttpServlet {
 //            raw_year == 
 //        } else {
 //        }
-//        request.getRequestDispatcher("../view/bill/search.jsp").forward(request, response);
+        request.getRequestDispatcher("../view/bill/search.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
